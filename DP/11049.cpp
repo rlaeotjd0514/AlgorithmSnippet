7c044ad942c3be8e6b530979ba570a2e793d5f38
대성 김 <fizz10wkddls@naver.com>
2020-05-22 20:03:00 +0900

    11049 solved

--- DP/11049.cpp
+++ DP/11049.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 struct met {
 	int x, y;
-	unsigned int sum = 1;
+	unsigned long sum = 1;
 };
 
 met power(met a, met b) {
-	if (a.y != b.x) return { -1, -1, (unsigned int)-1 };
-	return { a.x, b.y, (unsigned int)(a.x * a.y * b.y) };
+	if (a.y != b.x) return { -1, -1, (unsigned long)-1 };
+	return { a.x, b.y, (unsigned long)(a.x * a.y * b.y) };
 }
 
 met dp[501][501];
+long cost[501][501];
 
 int main()
 {
@@ -30,7 +31,7 @@ int main()
 
 	for (int i = n - 1; i >= 1; i--) {
 		for (int j = 1; j <= i; j++) {
-			met sv = { 0, 0, 0xffffffffui32 };
+			met sv = { 0, 0, 0xffffffff };
 			int x = -1, y = -1;
 			for (int k = 1; k <= n - i; k++) {
 				int x1 = i + k;
@@ -39,6 +40,10 @@ int main()
 				int y2 = j + (n - i) - k + 1;
 				met s = power(dp[x1][y1], dp[x2][y2]);
 				if (s.sum == -1) continue;
+				int s1, s2;
+				s1 = x1 == n ? 0 : dp[x1][y1].sum;
+				s2 = x2 == n ? 0 : dp[x2][y2].sum;
+				s.sum += s1 + s2;
 				if (sv.sum > s.sum) {
 					sv = s;
 				}
@@ -46,13 +51,31 @@ int main()
 			dp[i][j] = sv;
 		}
 	}
+	for (int i = 1; i <= n - 1; i++) {
+		cost[n - 1][i] = dp[n - 1][i].sum;
+	}
+	for (int i = n - 2; i >= 1; i--) {
+		for (int j = 1; j <= i; j++) {
+			int p1 = cost[i + 1][j] + dp[i][j].sum;
+			int p2 = cost[i + 1][j + 1] + dp[i][j].sum;
+			cost[i][j] = p1 < p2 ? p1 : p2;
+		}
+	}
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
 			cout << "{" << dp[i][j].x << "," << dp[i][j].y << ":" << dp[i][j].sum << "} ";
+			//cout << dp[i][j].sum << " ";
 		}
 		cout << "\n";
 	}
+
+	/*for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << cost[i][j] << " ";
+		}
+		cout << "\n";
+	}*/
 
 	cout << dp[1][1].sum;
 
