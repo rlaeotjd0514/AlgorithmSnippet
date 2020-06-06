9833dacebd0b29e70e4584f0c4c4f19167589ca4
대성 김 <fizz10wkddls@naver.com>
2020-06-06 13:01:27 +0900

    5582

--- DP/2169.cpp
+++ DP/2169.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+<<<<<<< HEAD
+#include <string>
+=======
+>>>>>>> 5582
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+<<<<<<< HEAD
+int dp[4001][4001];
+
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    int l1 = s1.length();
+    int l2 = s2.length();
+    int mx = 0;
+    for (int i = 1; i <= l1; i++) {
+        for (int j = 1; j <= l2; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (mx < dp[i][j]) mx = dp[i][j];
+            }
+        }
+    }
+    cout << mx;
+    return 0;
+}
+=======
 int main()
 {
     int n, m;
@@ -48,3 +74,4 @@ int main()
 
     return 0;
 }
+>>>>>>> 5582
